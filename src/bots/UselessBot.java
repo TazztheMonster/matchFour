42b79416adf24a5ca560a4f3e1d5d779a6adba18
bluez <bluez@192.168.2.134>
2021-06-08 19:42:48 +0200

    created java class

---
+++ src/bots/UselessBot.java
@@ -0,0 +1,23 @@
+package bots;
+
+public class UselessBot implements MatchFourBot{
+
+	@Override
+	public int turn(char[][] board, char icon) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void won(boolean won) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getBotName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
