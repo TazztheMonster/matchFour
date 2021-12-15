37c15586dae70aec398d12e81b432302ddc7e32e
Felix Pape <Felix_Pape@ymail.com>
2021-12-15 19:20:35 +0100

    Removes chaosBot from default competition

--- src/application/Application.java
+++ src/application/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Competition competition = new Competition(new AssholeBot(), new ChaosBot(), new EuloBot(), new ExampleBot(), new PlebBot(), new RandomColumnFillerBot());
+        Competition competition = new Competition(new AssholeBot(), new EuloBot(), new ExampleBot(), new PlebBot(), new RandomColumnFillerBot());
         competition.compete(50);
         System.out.println(competition.getResultsAsString());
 
